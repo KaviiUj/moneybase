db7556246ef52a6593c9b54897afa60aa2ffaa76
KaviiUj <kavii.umayanga0425@gmail.com>
2025-07-16 21:45:57 +0530

    added delay to refresh every 8 second

--- app/src/main/java/com/example/myapplication/presentation/market/MarketViewModel.kt
+++ app/src/main/java/com/example/myapplication/presentation/market/MarketViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.viewModelScope
 import com.example.myapplication.domain.marketSummary.GetMarketSummaryUseCase
 import com.example.myapplication.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,20 +22,33 @@ class MarketViewModel @Inject constructor(
     val state = _state.asStateFlow()
 
     init {
-        getMarketSummary()
+        viewModelScope.launch {
+            while (true) {
+                getMarketSummary()
+                delay(8000)
+            }
+        }
     }
 
     private fun getMarketSummary() {
+        _state.value = _state.value.copy(isLoading = true, result = emptyList())
         getMarketSummaryUseCase().onEach { result ->
             when(result) {
                 is Resource.Loading -> {
-                    _state.value = MarketState(isLoading = true)
+                    _state.value = _state.value.copy(isLoading = true)
                 }
                 is Resource.Success -> {
-                    _state.value = MarketState(result = result.data ?: emptyList())
+                    _state.value = _state.value.copy(
+                        result = result.data ?: emptyList(),
+                        isLoading = false,
+                        error = null
+                    )
                 }
                 is Resource.Error -> {
-                    _state.value = MarketState(error = result.message ?: "An unexpected error occurred")
+                    _state.value = _state.value.copy(
+                        isLoading = false,
+                        error = result.message
+                    )
                 }
             }
         }.launchIn(viewModelScope)
