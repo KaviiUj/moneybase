f4b66d43dd4b5a7c4757f5bc5652956bf63e71d3
KaviiUj <kavii.umayanga0425@gmail.com>
2025-07-16 22:55:37 +0530

    code optimized

--- app/src/main/java/com/example/myapplication/domain/marketSummary/MarketSummaryRepo.kt
+++ app/src/main/java/com/example/myapplication/domain/marketSummary/MarketSummaryRepo.kt
@@ -1,8 +1,6 @@
 package com.example.myapplication.domain.marketSummary
 
 import com.example.myapplication.domain.marketSummary.model.Result
-import com.example.myapplication.util.Resource
-import kotlinx.coroutines.flow.Flow
 
 interface MarketSummaryRepo {
     suspend fun getMarketSummary(): List<Result>
